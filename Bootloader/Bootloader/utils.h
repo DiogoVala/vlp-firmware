bd73896c869332f61dd004d127a596d83f2e06f5
DiogoVala <diogo.vala@ua.pt>
2021-10-09 13:59:14 +0100

    Bootloader -RADIO_UART

---
+++ Bootloader/Bootloader/utils.h
@@ -0,0 +1,21 @@
+/* utils.h
+ * Forked from Pedro Rodrigues' vlp-setup-firmware
+ * https://github.com/m-rego/vlp-setup-firmware
+ * 
+ * Updated by: Diogo Vala
+ *
+ * Description: Useful macro functions
+ * 
+ */
+
+#ifndef __UTILS_H__
+#define	__UTILS_H__
+
+/* Macro functions d*/
+#define set_bit(REGISTER, BIT)    REGISTER |=  _BV(BIT)
+#define clr_bit(REGISTER, BIT)    REGISTER &= ~(_BV(BIT))
+#define check_bit(REGISTER, BIT) (REGISTER & (_BV(BIT))) >> BIT
+#define xor_bit(REGISTER, BIT)    REGISTER ^= _BV(BIT)
+
+#endif
+
