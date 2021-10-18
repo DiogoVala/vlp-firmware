4495416fed37b0b2293c6bcdb033a976ed5694d2
DiogoVala <diogo.vala@ua.pt>
2021-10-18 12:18:54 +0100

    Flasher should be working

--- OTA_Flasher/OTA_Flasher/main.c
+++ OTA_Flasher/OTA_Flasher/main.c
@@ -22,12 +22,9 @@
 #include "../../Common/spi.h"
 #include "../../Common/nrf24l01.h"
 
-#define LED_PIN     PINB
-#define LED	    PINB0
-
 #define	MAX_PLD_SIZE (NRF24_MAX_PAYLOAD - 1) /* Number of data bytes in the payload */
 #define PKT_ID_IDX 0 /* First byte of the payload is reserved for the packet identifier */
-#define PKT_ID_SIZE 1 /* Size of the packet identifier (in bytes) */
+#define PKT_DATA_START 1 /* Size of the packet identifier (in bytes) */
 #define FIFO_SIZE 256 /* Number of bytes in the data buffer that stores received bytes from UART */
 #define TX_TIMEOUT 50000 /* Some generic timeout value if there is no transmission for a while */
 
@@ -41,14 +38,14 @@ static void rf_to_uart(void);
 uint8_t min(uint8_t x, uint8_t y);
 
 /* FIFO to receive data from UART ISR */
-static volatile struct fifo_buffer_s {
+static struct ring_buffer_s {
 	uint8_t data[FIFO_SIZE];
-	uint16_t len; /* Number of bytes currently stored */
-} tx_fifo;
+	uint8_t len; /* Number of bytes currently stored */
+} ring_buf;
 
 /* Function to fill the FIFO */
 static void handle_input(volatile uint8_t ch) {
-	tx_fifo.data[tx_fifo.len++] = ch;
+	ring_buf.data[ring_buf.len++] = ch;
 }
 
 int main() {
@@ -92,7 +89,7 @@ static void rf_to_uart(void){
 		if (rx_pkt_buf[0] != pkt_id) {
 
 			pkt_id = rx_pkt_buf[0];
-			for (uint8_t i = PKT_ID_SIZE; i < rx_pkt_len; i++)
+			for (uint8_t i = PKT_DATA_START; i < rx_pkt_len; i++)
 			{
 				uart_putc(rx_pkt_buf[i]); /* Redirect to UART */
 			}
@@ -103,11 +100,12 @@ static void rf_to_uart(void){
 static void uart_to_rf(void) {
 
 	static bool first_tx = true;
-	static uint8_t pkt_id = 0; /* Number of the packet we are currently sending */
-	static uint8_t tx_pkt_len;
-	static uint8_t tx_pkt_buf[NRF24_MAX_PAYLOAD];
 	static uint32_t first_tx_timeout = TX_TIMEOUT;
 	uint32_t tx_retries = 50;
+	
+	static uint8_t pkt_id = 0; /* Number of the packet we are currently sending */
+	uint8_t pkt_len;
+	uint8_t pkt_buf[NRF24_MAX_PAYLOAD];
 	
 	/* If there is no transmission for a while, start over */
 	if (!first_tx_timeout--)
@@ -131,25 +129,27 @@ static void uart_to_rf(void) {
 		
 		first_tx = false;
 	}
-	else if (tx_fifo.len){ /* If there is data in the FIFO */
+	else if (ring_buf.len){ /* If there is data in the FIFO */
 
 		/* First byte of the buffer has the packet identifier  */
-		tx_pkt_buf[PKT_ID_IDX] = pkt_id ++;
+		pkt_buf[PKT_ID_IDX] = pkt_id ++;
+		
+		/* Accessing ring_buf.len should be atomic */
+		pkt_len = min(ring_buf.len, MAX_PLD_SIZE); /* Number of bytes to send [1-31] */
 
-		/* UART may be placing byte into the buffer, so disabling interrupt is needed */
-		cli();
-		tx_pkt_len = min(tx_fifo.len, MAX_PLD_SIZE); /* Number of bytes to send*/
+		cli(); /* Mutual exclusion with UART interrupt */
 		
-		memcpy(tx_pkt_buf+PKT_ID_SIZE, tx_fifo.data, tx_pkt_len); /* Copy bytes from FIFO into the payload array */
+		memcpy(pkt_buf+PKT_DATA_START, ring_buf.data, pkt_len); /* Copy bytes from FIFO into the payload array */
 		
-		memmove(tx_fifo.data, tx_fifo.data+tx_pkt_len, tx_fifo.len); /* Shift FIFO data to the start position */
+		memmove(ring_buf.data, ring_buf.data+pkt_len, FIFO_SIZE-pkt_len); /* Shift FIFO data to the start position */
 		
-		tx_fifo.len -= tx_pkt_len;
-		sei();
+		ring_buf.len -= pkt_len;
+		
+		sei(); /* End of exclusion */
 		
 		while (tx_retries--) { /* Send until received or timeout */
 
-			nrf24_sendData(tx_pkt_buf, tx_pkt_len);
+			nrf24_sendData(pkt_buf, pkt_len);
 			if (nrf24_wait_tx_result() == NRF24_MESSAGE_SENT)
 				break;
 			
